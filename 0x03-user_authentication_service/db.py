63a7f739e62649bf3a195f22c257279bbd7ec41c
oekenonu <146424918+oekenonu@users.noreply.github.com>
2024-09-12 13:05:32 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,3 +55,21 @@ class DB:
         except InvalidRequestError:
             raise InvalidRequestError("Invalid query arguments provided.")
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
+        self._session.commit()
